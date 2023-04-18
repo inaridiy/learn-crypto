320801b3f97c32af28167607262ba74efb119870
Inaridiy <inainainariinainari@gmail.com>
2023-04-19 02:49:57 +0900

    feat

--- utils/print.ts
+++ utils/print.ts
@@ -1,5 +1,10 @@
 const SPLITTER_LENGTH = 64;
 
+const printableTransform = (x: any) => {
+  if (typeof x === "bigint") return "0x" + x.toString(16);
+  return x;
+};
+
 const printRecordNested = (title: string, x: Record<string, any>, depth: number) => {
   if (title.length === 0) console.log("=".repeat(SPLITTER_LENGTH));
   else console.log("== " + title + " " + "=".repeat(SPLITTER_LENGTH - title.length - 4));
@@ -7,10 +12,10 @@ const printRecordNested = (title: string, x: Record<string, any>, depth: number)
   for (const key in x) {
     if (typeof x[key] === "object") {
       for (const key2 in x[key]) {
-        console.log(`${key}.${key2}: ${x[key][key2]}`);
+        console.log(`${key}.${key2}: ${printableTransform(x[key][key2])}`);
       }
     } else {
-      console.log(`${key}: ${x[key]}`);
+      console.log(`${key}: ${printableTransform(x[key])}`);
     }
   }
   console.log("=".repeat(SPLITTER_LENGTH));
