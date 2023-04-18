3afbcf943cc4c425e4c2b66d0ade4665c50e1bb8
Inaridiy <inainainariinainari@gmail.com>
2023-04-19 02:36:54 +0900

    feat: ECDH

--- topics/ecdh.ts
+++ topics/ecdh.ts
@@ -2,13 +2,20 @@ import { print } from "../utils/print";
 import { EllipticCurve } from "./EllipticCurve";
 import { random } from "./random";
 
-const BIG_NUM = 2n ** 64n;
+const BIG_NUM = 2n ** 256n;
 
 const curve = EllipticCurve.SECP256K1;
 const a = random(BIG_NUM);
 const b = random(BIG_NUM);
 
-print("a", { a, b });
+print("Private", { a, b });
 
-const aP = curve.multiply(EllipticCurve.SECP256K1_G, 7n);
-// const bP = curve.multiply(EllipticCurve.SECP256K1_G, 12n);
+const aP = curve.multiply(EllipticCurve.SECP256K1_G, a);
+const bP = curve.multiply(EllipticCurve.SECP256K1_G, b);
+
+print("Public", { aP, bP });
+
+const baP = curve.multiply(aP, b);
+const abP = curve.multiply(bP, a);
+
+print("Shared", { baP, abP, OK: baP.x === abP.x });
