87b919c35c22ae4aa6d5887f0a4cadeb57229700
Inaridiy <inainainariinainari@gmail.com>
2023-04-24 00:14:07 +0900

    feat: add comment

--- topics/simple-ec/ecdh.ts
+++ topics/simple-ec/ecdh.ts
@@ -4,18 +4,19 @@ import { random } from "./../random";
 
 const BIG_NUM = 2n ** 256n;
 
-const curve = EllipticCurve.SECP256K1;
-const a = random(BIG_NUM);
-const b = random(BIG_NUM);
+const curve = EllipticCurve.SECP256K1; //標準的な楕円曲線,a=0,b=7
+const G = EllipticCurve.SECP256K1_G; //基点
+const a = random(BIG_NUM); //Aliceの秘密鍵
+const b = random(BIG_NUM); //Bobの秘密鍵
 
 print("Private", { a, b });
 
-const aP = curve.multiply(EllipticCurve.SECP256K1_G, a);
-const bP = curve.multiply(EllipticCurve.SECP256K1_G, b);
+const aP = curve.multiply(G, a); //Aliceの公開鍵
+const bP = curve.multiply(G, b); //Bobの公開鍵
 
 print("Public", { aP, bP });
 
-const baP = curve.multiply(aP, b);
-const abP = curve.multiply(bP, a);
+const baP = curve.multiply(aP, b); //共有鍵 aP * b = bP * a (交換法則)
+const abP = curve.multiply(bP, a); //共有鍵
 
 print("Shared", { baP, abP, OK: baP.x === abP.x });
