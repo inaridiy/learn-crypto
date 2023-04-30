9f1b3ff65dec2820110873eba465edd12fe5e9ec
Inaridiy <inainainariinainari@gmail.com>
2023-04-30 23:22:53 +0900

    yeah

--- topics/simple-ec/EllipticCurve/EllipticCurve.test.ts
+++ topics/simple-ec/EllipticCurve/EllipticCurve.test.ts
@@ -4,7 +4,8 @@ it("Test Curve", () => {
   const curve = new EllipticCurve(4n, 14n);
   const point1 = { x: -1n, y: 3n };
   expect(curve.isOnCurve(point1)).toBe(true);
-  expect(curve.isOnCurve({ x: 0n, y: 0n })).toBe(false);
+  expect(curve.isOnCurve({ x: 0n, y: 0n })).toBe(true);
+  expect(curve.isOnCurve({ x: 0n, y: 1n })).toBe(false);
   expect(curve.isOnCurve(curve.add(point1, point1))).toBe(true);
   expect(curve.isOnCurve(curve.add(point1, point1, point1, point1))).toBe(true);
   expect(curve.multiply(point1, 16n)).toEqual(curve.add(...Array(16).fill(point1)));
