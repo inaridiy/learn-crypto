a4511f671d711701a7b40180b32a7bcf264761f6
Inaridiy <inainainariinainari@gmail.com>
2023-08-03 18:33:44 +0900

    impl pse module1

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  preset: "ts-jest/presets/default-esm",
-  globals: {
-    "ts-jest": {
-      useESM: true,
-    },
-  },
-};
